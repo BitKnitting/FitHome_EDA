4d419cd20679dcc2716c269baf84633c4c52cebd
Bitknitting <bitknitting@gmail.com>
2020-02-25 04:13:47 -0800

    added requirements.txt

--- energy_leaks.py
+++
@@ -1,9 +0,0 @@
-from readings.readings import PowerReadings
-
-class EnergyLeaks:
-    @property
-    ######################################################
-    # The amount is calculated by looking at the distribution
-    # of values.  The energy leak value will be 
-    def amount(self):
-        p = PowerReadings()
